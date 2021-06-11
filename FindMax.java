16ba243307205d2a01235616237b9c0384924668
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-11 21:02:14 +0530

    refactor: To generic class to find max value

--- FindMax.java
+++ FindMax.java
@@ -10,24 +10,39 @@
 
 package javaGenerics;
 
-	
-public class FindMax {
-	/**
-	 * Refactor 1- Generic Method to Find the Max Value of passed Arguments
-	 */
-	 public <E extends Comparable> E findMaxValue(E firstValue, E secondValue, E thirdValue){
-	     E max = firstValue;
-	     if (secondValue.compareTo(max) > 0) {
-	         max = secondValue;
-	     }
-	     if (thirdValue.compareTo(max) > 0) {
-	         max = thirdValue;
-	     }
-	     printMax(max);
-	     return max;
-	 }
-	 
-	 public <E> void printMax(E max){
-	     System.out.println("Max Value = "+max);
-	 }
- }
+/**
+ * Refactor-2 created generic class eith parameter constructor extends comparable
+ * @param <E>
+ */
+
+public class FindMax<E extends Comparable<E>> {
+	E[] values;
+
+	public FindMax(E[] values) {
+		this.values = values;
+	}
+
+	public static <E> void printMax(E max) {
+		System.out.println("Max Value = " + max);
+	}
+
+	public static void main(String[] args) {
+		Integer[] integerValues = { 50, 20, 300 };
+		String[] stringValues = { "All", "Good", "Morning" };
+		new FindMax<String>(stringValues).findMaxValue();
+		new FindMax<Integer>(integerValues).findMaxValue();
+	}
+
+	public E findMaxValue() {
+		if (values[0].compareTo(values[1]) > 0 && values[0].compareTo(values[2]) > 0) {
+			printMax(values[0]);
+			return values[0];
+		} else if (values[1].compareTo(values[0]) > 0 && values[1].compareTo(values[2]) > 0) {
+			printMax(values[1]);
+			return values[1];
+		} else {
+			printMax(values[2]);
+			return values[2];
+		}
+	}
+}
