9c09139317cf976de07e9e2f84817531db58e7a1
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-12 23:05:21 +0530

    added: method to insert given elements

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -7,8 +7,6 @@
  * 
  */
 
-
-
 package datastructure.java;
 
 public class LinkedList<T> {
@@ -17,14 +15,27 @@ public class LinkedList<T> {
 		private LinkedListNode<T> first = null;
 		
 		/**
-		 * Method to insert element in link list
+		 * UC-1  Method to insert element in link list
 		 * @param node
 		 */
 		public void insert(LinkedListNode<T> node) {
 			node.setNext(first);
 			first = node;
 		}
-		
+			
+
+		/**
+		 * UC-2 Recursively traverse this list and print the node value
+		 * @param node
+		 */
+		private void printList(LinkedListNode<T> node) {
+			System.out.println("Node is " + node.getValue());
+			if(node.getNext()!=null) printList(node.getNext());
+		}
+
+		public void print(){
+			printList(first);
+		}
 		
 		public static void main(String[] args) {
 			LinkedList<Integer> list = new LinkedList<Integer>();
@@ -32,6 +43,7 @@ public class LinkedList<T> {
 			list.insert(new LinkedListNode<Integer>(30));
 			list.insert(new LinkedListNode<Integer>(70));
 			
+			list.print();
 		}
 
 }
