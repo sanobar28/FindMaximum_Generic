7a688a665cb9b39f4545b68255b04ffcfdd31b9b
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-13 08:22:15 +0530

    added: insert elements at desired position

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -10,40 +10,47 @@
 package datastructure.java;
 
 public class LinkedList<T> {
+
+	LinkedListNode<T> head;
+	private LinkedListNode<T> first = null;
+
+	/**
+	 * UC-1 Method to insert element in link list
+	 * 
+	 * @param node
+	 */
+	public void insert(LinkedListNode<T> node) {
+		node.setNext(first);
+		first = node;
+	}
+
+	/**
+	 * UC-2 Recursively traverse this list and print the node value
+	 * 
+	 * @param node
+	 */
+	private void printList(LinkedListNode<T> node) {
+		System.out.println("Node is " + node.getValue());
+		if (node.getNext() != null)
+			printList(node.getNext());
+	}
+
+	public void print() {
+		printList(first);
+	}
 	
+	/**
+	 * UC-3 & UC-4 to insert elements at desired positions
+	 * @param args
+	 */
 
-		private LinkedListNode<T> first = null;
-		
-		/**
-		 * UC-1  Method to insert element in link list
-		 * @param node
-		 */
-		public void insert(LinkedListNode<T> node) {
-			node.setNext(first);
-			first = node;
-		}
-			
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.insert(new LinkedListNode<Integer>(70));
+		list.insert(new LinkedListNode<Integer>(30));
+		list.insert(new LinkedListNode<Integer>(56));
 
-		/**
-		 * UC-2 Recursively traverse this list and print the node value
-		 * @param node
-		 */
-		private void printList(LinkedListNode<T> node) {
-			System.out.println("Node is " + node.getValue());
-			if(node.getNext()!=null) printList(node.getNext());
-		}
-
-		public void print(){
-			printList(first);
-		}
-		
-		public static void main(String[] args) {
-			LinkedList<Integer> list = new LinkedList<Integer>();
-			list.insert(new LinkedListNode<Integer>(56));
-			list.insert(new LinkedListNode<Integer>(30));
-			list.insert(new LinkedListNode<Integer>(70));
-			
-			list.print();
-		}
+		list.print();
+	}
 
 }
